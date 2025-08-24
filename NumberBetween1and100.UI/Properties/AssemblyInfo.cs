8b7953c059994b7f5bfbfcc951b3cd02333f8344
Dilip Kumar <123343100+Dilip-996@users.noreply.github.com>
2025-08-24 14:22:38 -0400

    Add WinForms UI for divisible by three count

---
+++ NumberBetween1and100.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NumberBetween1and100.UI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NumberBetween1and100.UI")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f2c096dd-9ebd-4665-a44e-48c8b1894dd5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
